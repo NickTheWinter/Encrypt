c9a9dc53e96d5b6d5d30cfab2fb4f562c48b417c
NickTheWinter <k.zimenkov@mail.ru>
2022-03-15 11:16:44 +0300

    add project

---
+++ Encript/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encript
+{
+    internal class Program
+    {
+        static int isSimple(int number)
+        {
+            if (number == 1)
+                return 0;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if ((number % i == 0))
+                    return 0;
+            }
+            return 1;
+        }
+        static int nSimple(int n)
+        {
+            int i = 0, nuber = 0;
+            while (i < n)
+            {
+                nuber++;
+                i += isSimple(nuber);
+            }
+            return nuber;
+        }
+        static int ostat(int x, int y, long z)
+        {
+            int ost = x;
+            for (int i = y - 1; i > 0; i--)
+            {
+                ost = (ost * x) % Convert.ToInt32(z);
+            }
+            return ost;
+        }
+        static int closeKey(int p, int q, int e)
+        {
+            ulong fi = Convert.ToUInt32((p - 1) * (q - 1));
+            int d = 0;
+            ulong ost = 0;
+            while (ost != 1)
+            {
+                d++;
+                if (isSimple(d) == 1)
+                {
+                    ost = Convert.ToUInt32((e * d)) % fi;
+                }
+            }
+            return d;
+        }
+        static List<int> code (char[] c, int e, long n)
+        {
+            List<int> rez = new List<int>();
+
+            for (uint i = 0; i < c.Length; i++)
+            {
+                rez.Add((char)ostat(c[i], e, n));
+            }
+            return rez;
+        }
+        static List<char> decode(List<int> c, long d, long n)
+        {
+
+            List<char> rez = new List<char>();
+            for (int i = 0; i < c.Count; i++)
+            {
+                rez.Add((char)ostat(c[i], (int)d, n));
+            }
+            return rez;
+        }
+        
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int p = nSimple(29);
+            int q = nSimple(31);
+            long n = p * q;
+            int e = 257;
+            long d = closeKey(p, q, e);
+            int symbol = 666;
+            int shifr = ostat(symbol, e, n);
+            char[] phraze = "Привет, мир!".ToCharArray();
+            Console.Write("Исходный: ");
+            foreach (char c in phraze)
+            {
+                Console.Write(c);
+            }
+            Console.WriteLine();
+            List<int> codePhraze = code(phraze, e, n);
+            List<char> decoding = decode(codePhraze, d, n);
+
+            Console.Write("Зашифрованный: ");
+            for (int i = 0; i < codePhraze.Count; i++)
+            {
+                Console.Write($"{codePhraze[i]} ");
+            }
+            Console.WriteLine();
+            Console.Write("Расшифрованный: ");
+            foreach (char decode in decoding)
+            {
+                Console.Write(decode);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Encript/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Encript")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Encript")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c3153a71-f455-4e3b-8270-8a3bcb74e682")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
